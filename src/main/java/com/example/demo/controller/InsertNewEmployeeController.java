3c63fc577b7d66c7624fb045e329a88977b5efa8
Jitendra singh <jitendrasingh25889@gmail.com>
2024-01-06 21:12:47 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/InsertNewEmployeeController.java
@@ -0,0 +1,32 @@
+ package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/employees")
+public class InsertNewEmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@GetMapping("/add")
+	public String addEmployee(@RequestParam String name, @RequestParam String email, @RequestParam Float sal, @RequestParam String addr) {
+		Employee employee = new Employee();
+		
+		employee.setEadd(addr);
+		employee.setEname(name);
+		employee.setEmail(email);
+		employee.setEsal(sal);
+		
+		employeeRepository.save(employee);
+		
+		return "success";
+	}
+}
